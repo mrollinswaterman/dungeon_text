d2068831b4fc4e5407acd71f4eb961bc907f93af
Miles Rollins-Waterman <mrollinswaterman@uchicago.edu>
2024-02-14 08:29:17 -0600

    added Item + weapon class

--- dungeon_text.py
+++ dungeon_text.py
@@ -21,8 +21,6 @@ class Player():
     def __init__(self, name: str):
         self.name = name
 
-
-
         self._stat_map = {
             "str": self._strength,
             "dex": self._dexterity,
@@ -45,6 +43,7 @@ class Player():
         self._hp = self._max_hp
         self._evasion = 10 + BONUS[self._dexterity]
         self._xp = 0
+        self._level = 1
         self._gold = 0
         self._inventory = []
 
@@ -55,7 +54,10 @@ class Player():
     #properties
     @property
     def dead(self):
-        return self._hp < 0
+        return self._hp <= 0
+    @property
+    def level(self):
+        return self.level
     @property
     def str(self) -> int:
         return self._strength
@@ -139,3 +141,47 @@ class Player():
 
         else:
             raise ValueError("Can't drop an item you don't have")
+
+class Item():
+
+    def __init__(self, id, rarity):
+
+        #rarity map
+        self._id = id
+        self._rarity = rarity
+        self._value = 5 * rarity
+
+    #properties
+    @property
+    def id(self):
+        return self._id
+    @property
+    def value(self):
+        return self._value
+    
+
+class Weapon(Item):
+
+    def __init__(self, id, rarity, damage_dice: int):
+        super().__init__(id, rarity)
+        self._damage_dice = damage_dice
+
+        self._max_durability = 10 * self._rarity
+        self._durability = self._max_durability
+
+    #properties
+    @property
+    def broken(self):
+        return self._durability <= 0
+    @property
+    def durability(self):
+        return self._durability
+    
+    def lose_durability(self) -> None:
+        self._durability -= 1 
+
+
+sword = Weapon("weapon", 1, 8)
+
+print(sword.value)
+
