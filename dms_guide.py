e290c9fbd10d5251f6a961f2b49bc931d0183718
wast3land <miles.r.waterman@gmail.com>
2024-02-17 02:22:55 -0600

    added event functionality

--- dms_guide.py
+++ dms_guide.py
@@ -1,22 +1,31 @@
 import events
 
+boulder = events.Event()
 
-STRENGTH_LINES = [
-    f'A boulder blocks your way.',
+boulder.add_stat(("str", 10))
+boulder.add_stat(("dex", 20))
 
-    f'You come across a gnomish miner. He is struggling 
-    to break a rock with a pickaxe that is far too large for his small frame.',
+boulder.add_tries(2)
 
-    f'You come to the edge of a small chasm. It looks jumpable...',
+boulder.add_text(f'A boulder blocks your way.')
 
-    f'The path abruptly ends in a sheer rock wall.'
-]
+boulder.add_message((True, "str", f"\nYou push the boulder aside and continue on your way."))
+boulder.add_message((True, "str", f"\nYou smash the boulder to rubble for daring to stand in your way."))
+boulder.add_message((True, "dex", f"\nYou manage to squeeze by the boulder."))
+boulder.add_message((True, "dex", f"\nYou're flexible enough to make it past."))
 
-EVENTS_LIST = []
+boulder.add_message((False, "str", f"\nYou push with all your might, but the boulder doesn't budge. You'll need to push a little harder"))
+boulder.add_message((False, "str", f"\nYou punch the boulder. Nothing happens. Surely punching it harder will do something!"))
+boulder.add_message((False, "dex", f"\nYou do a sick spin move. The boulder is unimpressed. Maybe try squeezing past it?"))
+boulder.add_message((False, "dex", f"\nYou try to squeeze past, but don't quite make it. You wriggle yourself out before you get stuck."))
 
-check = events.StatCheck_Event(
-    {"str": (10, [], []), "dex": (20, [], [])}
-)
+print(boulder.run("con", 22))
+print(boulder.run("con", 22))
+print(boulder.run("con", 22))
 
-[f"You push the boulder aside and continue on your way",
-                  f"You pummel the boulder to rubble for daring to stand in your way."]
+
+#STRENGTH_LINES = [f'You come across a gnomish miner. He is struggling to break a rock with a pickaxe that is far too large for his small frame.', f'You come to the edge of a small chasm. It looks jumpable...',
+
+    #f'The path abruptly ends in a sheer rock wall.'
+#]
+
--- events.py
+++ events.py
@@ -4,147 +4,98 @@ import random
 
 FAILURE_LINES = {
     "str": [
-        f"You're going to need more than brawn to solve this one."
+        f"\nYou're going to need more than brawn to solve this one."
     ],
 
     "dex": [
-        f"You do a sick spin move. Nothing happens."
+        f"\nYou do a sick spin move. Nothing happens."
     ],
 
     "con": [
-        f"After the 5th consecutive minute of holding your breath, you give up."
+        f"\nAfter the 5th consecutive minute of holding your breath, you give up."
     ],
 
     "int": [
-       f"Can't think your way out of this one nerd!"
+       f"\nCan't think your way out of this one nerd!"
     ],
 
     "wis": [
-        f"You pause for a while to ponder the forms. The forms are throughly unhelpful."
+        f"\nYou pause for a while to ponder the forms. The forms are throughly unhelpful."
     ],
 
     "cha": [
-        f"You tell a mildly amusing Knock, Knock joke. Nobody laughs."
-    ]
-}
-
-SUCCESS_LINES = {
-    "str": [
-        f"Another win for the big guys!"
-    ],
-
-    "dex": [
-        f"Seems those ballet classes paid off after all"
-    ],
-
-    "con": [
-        f"No one is sure exactly what you did, but you sure did it!"
-    ],
-
-    "int": [
-        f"Brains wins the day!"
-    ],
-
-    "wis": [
-        f"You've been in situations like this before and this was no more difficult."
-    ],
-
-    "cha": [
-        f"You don't need money when you look like that, do ya?"
+        f"\nYou tell a mildly amusing Knock, Knock joke. Nobody laughs."
     ]
 }
 
 END_LIST = [
-    f"You can't try this anymore."
+    f"\nYou can't try this anymore.", f"\nNo more tries left."
 ]
 
 
-class StatCheck_Event():
+class Event():
 
-    def __init__(self, difficulties_classes:dict[str, tuple], tries, 
-                 dialogue:str = "", failure_dictionary:dict[str, list[str]] = FAILURE_LINES,
-                 success_dictionary: dict[str, list[str]] = SUCCESS_LINES, end_list:list[str] = END_LIST):
+    def __init__(self):
 
-        self._stats:str = list(difficulties_classes.keys())
-        self._stat_codes = []
-        for stat in list(difficulties_classes.keys()):
-            self._stat_codes.append(stat.lower()[0:2])
-        self._difficulty_classes = difficulties_classes
-        self._text:str = dialogue
-        self._tries = tries
-        self._reward = None
-        self._has_been_passed = False
-        self._failure_dictionary = failure_dictionary
-        self._success_dictionary = success_dictionary
-        self._failure_text = {}
-        for stat in difficulties_classes:
-            if stat not in self._failure_text:
-                self._failure_text[stat] = difficulties_classes[stat][2]
-        self._success_text = {}
-        for stat in difficulties_classes:
-            if stat not in self._success_text:
-                self._success_text[stat] = difficulties_classes[stat][1]
-        self._end_list = end_list
+        self._stats = set()
+        self._tries = 0
+        self._text = ""
+        self._messages: dict[bool, list[tuple[str, str]]] = {True: [], False: []}
 
     #properties
     @property
-    def stats(self) -> str:
+    def stats(self) -> None:
         return self._stats
     @property
-    def stat_codes(self) -> str:
-        return self._stat_codes
-    @property
-    def reward(self) -> int | tuple[int,int]:
-        return self._reward
-    @property
-    def tries(self) -> int:
-        return self._tries
-    @property
-    def done(self) -> bool:
-        return self._tries <= 0
-    @property
-    def has_been_passed(self) -> bool:
-        return self._has_been_passed
+    def completed(self) -> bool:
+        return self._tries == 0
     
     #methods
-    def passed(self, stat:str, check:int) -> bool:
-        """
-        Checks if a skill check was high enough to pass the event's difficulty clas
+    def add_stat(self, stat: tuple[str, int]) -> None:
+        self._stats.add(stat)
 
-        Returns a string containing the narrator's response, and bool indicating success (True)
-        or failure (False)
-        """
-        if self.done:
-            return random.choice(self._end_list)
-        if stat not in self._stats:
-            self._tries -= 1
-            if len(self._failure_text) > 0:
-                return random.choice(self._failure_text[stat])
-            return random.choice(self._failure_dictionary[stat])
-
-        if check >= self._difficulty_classes[stat]:
-            self._has_been_passed = True
-            if len(self._success_text) > 0:
-                return random.choice(self._success_text[stat])
-            return random.choice(self._success_dictionary[stat])
-        
-        elif len(self._failure_text):
-            self._tries -= 1
-            return random.choice(self._failure_text[stat])
-
-        self._tries -= 1
-        return random.choice(self._failure_dictionary[stat])
+    def add_tries(self, tries:int) -> None:
+        self._tries = tries
     
-    def set_text(self, text:str) -> None:
+    def has_stat(self, stat:str) -> bool:
+        for pair in self._stats:
+            if pair[0] == stat:
+                return True
+        return False
+
+    def stat_dc(self, stat:str) -> None | tuple[str, int]:
+        for pair in self._stats:
+            if pair[0] == stat:
+                return self._stats[pair]
+            
+    def add_text(self, text:str) -> None:
         self._text = text
+
+    def add_message(self, message:tuple[bool, str, str]) -> None:
+        msg_type, stat, msg = message
+        self._messages[msg_type].append((stat, msg))
+
+    def run(self, stat:str, roll:int) -> None:
+
+        if self.completed is True:
+            return random.choice(END_LIST)
         
-    def set_reward(self, reward:int | tuple[int,int]) -> None:
-        self._reward = reward
+        self._tries -= 1
 
-    def set_failure_text(self, text:dict[str, str]) -> None:
-        self._failure_text = text
+        if self.has_stat(stat) is True:
+            for item in self._stats:
+                code, check = item
+                if code == stat and roll >= check:
+                    successes = []
+                    for msg in self._messages[True]:
+                        if msg[0] == stat:
+                            successes.append(msg[1])
+                    return random.choice(successes)
+            failures = []
+            for msg in self._messages[False]:
+                if msg[0] == stat:
+                    failures.append(msg[1])
 
-    def set_success_text(self, text:dict[str, str]) -> None:
-        self._success_text = text
+            return random.choice(failures)
 
-    
+        return random.choice(FAILURE_LINES[stat])
